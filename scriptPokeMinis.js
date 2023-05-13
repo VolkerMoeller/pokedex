21c827d9f8c9e79b4f5eb1843990abe0a23321e0
VolkerMoeller <v.moeller@t-online.de>
2023-05-13 10:41:31 +0200

    restored 2nd

--- scriptPokeMinis.js
+++ scriptPokeMinis.js
@@ -12,17 +12,26 @@ function showPokeCard(i) {
 
 
 async function initPokeMini() {
-    if (!functionRunning) {
-        functionRunning = true;
-        renderPokeMiniContent();
-        renderPokeMiniHeader();
-        getAndRenderPokeMini();
-        functionRunning = false;
-    }
+    if (pokes) {
+        console.log('hurray');
+        let end = pokes.length -1;
+        countX = end;
+    };
+    await renderPokeMini();
+    countX = 4;
 }
 
+async function renderPokeMini(){
+if (!functionRunning) {
+    functionRunning = true;
+    await renderPokeMiniContent();
+    await renderPokeMiniHeader();
+    await getAndRenderPokeMini();
+    functionRunning = false;
+}
+}
 
-function renderPokeMiniContent() {
+async function renderPokeMiniContent() {
     document.getElementById('pokedexMinis-all').innerHTML += generateHTMLPokeMiniContent();
 }
 
@@ -36,7 +45,7 @@ function generateHTMLPokeMiniContent() {
     `;
 }
 
-function renderPokeMiniHeader() {
+async function renderPokeMiniHeader() {
     document.getElementById('miniHeader').innerHTML = generateHTMLPokeMiniHeader();
 }
 
@@ -49,27 +58,26 @@ function generateHTMLPokeMiniHeader() {
 
 
 async function getAndRenderPokeMini() {
-    for (let j = 1; j <=countX; j++) {
-        let promises1st = [renderPokePlaces(nextPokeMini), getCurrentPoke(nextPokeMini)];
-        await Promise.all(promises1st);
-        let promises2nd = [takepokeMiniId(nextPokeMini), renderPokeMini(nextPokeMini)];
-        await Promise.all(promises2nd);
-        await getAndRenderImage(nextPokeMini);
-        await getAndRenderGermanName(nextPokeMini);
-        nextPokeMini++;
+        for (let j = 1; j <= countX; j++) {
+            let promises1st = [renderPokePlaces(nextPokeMini)];
+            await Promise.all(promises1st);
+            let promises2nd = [takepokeMiniId(nextPokeMini), renderPokeMiniNr(nextPokeMini)];
+            await Promise.all(promises2nd);
+            await getAndRenderImage(nextPokeMini);
+            await getAndRenderGermanName(nextPokeMini);
+            nextPokeMini++;
+        }
     }
-}
 
 
 async function getAndRenderImage(nextPokeMini) {
-    takeCurrentImageUrl();
-    renderCurrentImage(nextPokeMini, currentImageUrl);
+    renderCurrentImage(nextPokeMini);
     renderBackground(nextPokeMini);
 }
 
 
 function renderBackground(nextPokeMini) {
-    takeCurrentSlot1();
+    let miniSlot1 = pokes[nextPokeMini]['pokeSlot1'];
     let bgnSlotType = 'bgn-slot-type-' + miniSlot1;
     let bgnType = 'bgn-type-' + miniSlot1;
     document.getElementById('miniPokeCard' + nextPokeMini).classList.add(`${bgnType}`);
@@ -82,67 +90,12 @@ function takeCurrentSlot1() {
 
 
 async function getAndRenderGermanName(nextPokeMini) {
-    takeCurrentNamesUrl();
-    await getCurrentNames(currentNamesUrl);
-    takeCurrentGermanName()
-    renderCurrentGermanName(nextPokeMini, currentGermanName);
+    renderCurrentGermanName(nextPokeMini);
 }
 
-
-async function getCurrentPoke(nextPokeMini) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${nextPokeMini}/`;
-    let response = await fetch(url);
-    let responseAsJSON = await response.json();
-    rspCurrentPokeAsJSON = responseAsJSON;
-    return rspCurrentPokeAsJSON;
-}
-
-
-async function getCurrentNames(url) {
-    let response = await fetch(url);
-    let responseAsJSON = await response.json();
-    rspCurrentNamesAsJSON = responseAsJSON;
-    return rspCurrentNamesAsJSON;
-}
-
-
-async function takeCurrentImageUrl() {
-    currentImageUrl = rspCurrentPokeAsJSON['sprites']['other']['home']['front_default'];
-    return currentImageUrl;
-}
-
-
-async function takeCurrentNamesUrl() {
-    currentNamesUrl = rspCurrentPokeAsJSON['species']['url'];
-    return currentNamesUrl;
-}
-
-
-async function takeCurrentGermanName() {
-    currentNames = rspCurrentNamesAsJSON['names'];
-    await searchGermanNameMiniPoke(currentNames);
-}
-
-
-async function searchGermanNameMiniPoke(currentGermanNames) {
-    for (let j = 0; j < currentGermanNames.length; j++) {
-        let language = currentGermanNames[j]['language']['name'];
-        checkIfGerman(language, j);
-    }
-}
-
-
-function checkIfGerman(language, j) {
-    if (language == 'de') {
-        currentGermanName = currentNames[j]['name'];
-        return currentGermanName;
-    } else {
-        return;
-    }
-}
 
 function takepokeMiniId(nextPokeMini) {
-    pokeMiniId = rspCurrentPokeAsJSON['id'];
+    pokeMiniId = pokes['pokeId'];
     return pokeMiniId;
 }
 
@@ -165,19 +118,21 @@ function generateHTMLPlaces(nextPokeMini) {
 }
 
 
-async function renderPokeMini(nextPokeMini) {
+async function renderPokeMiniNr(nextPokeMini) {
     let formatedPokeMiniId = format3LeftHandZeros(nextPokeMini);
     document.getElementById('pokeMini' + nextPokeMini).innerHTML += generateHTMLpokeMiniId(formatedPokeMiniId);
 }
 
 
-async function renderCurrentImage(nextPokeMini, url) {
+async function renderCurrentImage(nextPokeMini) {
+    let url = pokes[nextPokeMini]['pokeImg'];
     document.getElementById('image' + nextPokeMini).innerHTML = generateHTMLImage(url);
 }
 
 
-async function renderCurrentGermanName(nextPokeMini, currentGermanName) {
-    document.getElementById('germanName' + nextPokeMini).innerHTML = generateHTMLGermanName(currentGermanName);
+async function renderCurrentGermanName(nextPokeMini) {
+    let name = pokes[nextPokeMini]['pokeName'];
+    document.getElementById('germanName' + nextPokeMini).innerHTML = generateHTMLGermanName(name);
 }
 
 
