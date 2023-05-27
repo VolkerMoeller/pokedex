9fd279c88ae9bb093d67b9c15d15cc3071bfa9fe
VolkerMoeller <v.moeller@t-online.de>
2023-05-27 17:43:46 +0200

    V 3.0 myPlace

--- script.js
+++ script.js
@@ -13,15 +13,11 @@ async function performServerRequests(i) {
     try {
         let url1 = `https://pokeapi.co/api/v2/pokemon/${i}/`;
         let url2 = `https://pokeapi.co/api/v2/pokemon-species/${i}/`;
-
         let arrayPokemon = await fetchDataFromServer(url1);
         console.log(i + ' pokemonData:', arrayPokemon);
-
         let arrayPokemonSpecies = await fetchDataFromServer(url2);
         console.log(i + ' pokemon-speciesData:', arrayPokemonSpecies);
-
         getAbiltiesData(arrayPokemon, i);
-
     } catch (error) {
         console.error('Fehler beim Ausführen der Serverzugriffe:', error);
     }
@@ -37,7 +33,6 @@ async function getAbiltiesData(arrayPokemon, i) {
 
 async function takeDynamikUrl(arrayPokemon) {
     let dynamicUrlIndex = arrayPokemon['abilities'].length - 1;
-    // nimm immer die letzte Url. Hier ist die ability nicht hidden
     let dynamicUrl = arrayPokemon['abilities'][dynamicUrlIndex]['ability']['url'];
     return dynamicUrl;
 }
@@ -61,7 +56,6 @@ async function performServerRequestBy(url) {
 
 async function getAbilityUrl() {
     let abilityUrl = arrayPokemon['abilities'][1]['ability']['url'];
-    // [1] hidden = false
     console.log('ablitityUrl: ', abilityUrl);
     await performServerRequestBy(abilityUrl);
     console.log('ablitityData: ', dataByUrl);
@@ -69,7 +63,7 @@ async function getAbilityUrl() {
 
 
 function test() {
-    for (let i = 1; i < 30; i++) {
+    for (let i = 1; i < 3; i++) {
         performServerRequests(i);
     }
 }
