2cfd98a0a343c96ac32f136b447a731db7393c7f
VolkerMoeller <v.moeller@t-online.de>
2023-05-29 14:40:42 +0200

    V 3.0 switchButton

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let initEnd = 50;
+let initEnd = 20;
 let nextPokeNr = initEnd + 1;
 let stepPokeNrs = 1;
 let endPokeNr = nextPokeNr + stepPokeNrs;
 let scrollCounter = 0;
 let maxPokeNr = 1010;
+let currentPokeNr = 0;
+let intervalId;
 
 
 async function getData() {
     document.getElementById('myPlace').innerHTML = '';
     for (let i = 1; i <= initEnd; i++) {
         await performServerRequests(i);
+        updateAmountPokesAndProgress(i);
     }
 }
 
@@ -18,9 +21,22 @@ function getNextData() {
     if (nextPokeNr <= maxPokeNr) {
         for (let j = nextPokeNr; j <= endPokeNr; j++) {
             performServerRequests(j);
+            updateAmountPokesAndProgress(j);
         }
         updateCountNrs();
     }
+}
+
+
+function startRepeatedExecution() {
+    switchButton();
+    intervalId = setInterval(getNextData, 5);
+}
+
+
+function stopRepeatedExecution() {
+    switchButton();
+    clearInterval(intervalId);
 }
 
 
@@ -58,6 +74,7 @@ async function performServerRequests(i) {
         let arrayPokemon = await fetchDataFromServer(url1);
         console.log(i + ' pokemonData:', arrayPokemon);
         renderPokeMinis1stLevel(i, arrayPokemon);
+        stylePokeBgn(i, arrayPokemon);
         let arrayPokemonSpecies = await fetchDataFromServer(url2);
         console.log(i + ' pokemon-speciesData:', arrayPokemonSpecies);
         getAbiltiesData(arrayPokemon, i);
@@ -112,6 +129,7 @@ async function renderPokeMinis1stLevel(i, arrayPokemon) {
     document.getElementById('myPlace').innerHTML += generateHTMLPokeMini(i, imgSrc, pokeId, pokeName);
 }
 
+
 function generateHTMLPokeMini(i, imgSrc, pokeId, pokeName) {
     return `
     <div id="pokeMini${i}" class="pokeMini">
@@ -124,4 +142,73 @@ function generateHTMLPokeMini(i, imgSrc, pokeId, pokeName) {
         </div>
     </div>
     `
+}
+
+
+// progressBar
+
+
+function updateAmountPokesAndProgress(currentPokeNr) {
+    renderAmountLoadedPokes(currentPokeNr);
+    updateProgress(currentPokeNr);
+}
+
+
+function renderAmountLoadedPokes(currentPokeNr) {
+    document.getElementById('amount-pokes-loaded').innerHTML = '';
+    document.getElementById('amount-pokes-loaded').innerHTML = generateHTMLAmountLoadedPokes(currentPokeNr);
+}
+
+
+function generateHTMLAmountLoadedPokes(currentPokeNr) {
+    return 'Es wurden ' + currentPokeNr + ' von 1010 geladen';
+}
+
+
+function updateProgress(currentPokeNr) {
+    let progressWidth = currentPokeNr / maxPokeNr * 100;
+    document.getElementById('progress').style = `width: ${progressWidth}%`;
+}
+
+
+// background-color
+
+
+
+async function stylePokeBgn(currentPokeNr, arrayPokemon) {
+    let pokeType = arrayPokemon['types'][0]['type']['name'];
+    setBgnByType(pokeType, currentPokeNr);
+}
+
+
+function setBgnByType(pokeType, i) {
+    document.getElementById('pokeMini' + i).classList.add('bgn-type-' + pokeType);
+}
+
+
+// switch button
+
+
+function switchButton() {
+    let loadBtnStop = document.getElementById('loadBtnStop');
+    let loadBtnStart = document.getElementById('loadBtnStart');
+    if (loadBtnStop.classList.contains('display-none') == true) {
+        displayOn(loadBtnStop);
+        displayOff(loadBtnStart);
+    } else {
+        displayOff(loadBtnStop);
+        displayOn(loadBtnStart);
+    }
+}
+
+
+function displayOn(element) {
+    element.classList.remove('display-none');
+    element.classList.add('display-flex');
+}
+
+
+function displayOff(element) {
+    element.classList.add('display-none');
+    element.classList.remove('display-flex');
 }
