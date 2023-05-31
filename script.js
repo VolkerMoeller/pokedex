1d0b48315208b3681446487aadcb243653d1cceb
VolkerMoeller <v.moeller@t-online.de>
2023-05-31 16:14:34 +0200

    V 3.0 search

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let initEnd = 20;
 let nextPokeNr = initEnd + 1;
-let stepPokeNrs = 49;
+let stepPokeNrs = 19;
 let endPokeNr = nextPokeNr + stepPokeNrs;
 let maxPokeNr = 1010;
 let currentPokeNr = 0
 let indexOfGermanData;
+let functionRunning = false;
+let loadedPokeNames = [];
+let loadedPokeIds = [];
 
 
 function init() {
+    loadedPokeNames = [];
+    loadedPokeIds = [];
     document.getElementById('myPlace').innerHTML = '';
     getData(1, initEnd);
 }
@@ -19,11 +24,15 @@ function initNext() {
 
 
 async function getData(begin, end) {
-    for (let i = begin; i <= end; i++) {
-        await performServerRequests(i);
-        updateAmountPokesAndProgress(i);
+    if (!functionRunning) {
+        functionRunning = true;
+        for (let i = begin; i <= end; i++) {
+            await performServerRequests(i);
+            updateAmountPokesAndProgress(i);
+        }
+        updateCountNrs(end);
+        functionRunning = false;
     }
-    updateCountNrs(end);
 }
 
 
@@ -60,9 +69,22 @@ async function performServerRequests(i) {
 async function fetchArrayPokemon(i, url1) {
     let arrayPokemon = await fetchDataFromServer(url1);
     console.log(i + ' pokemonData:', arrayPokemon);
+    useArrayPokemon(i, arrayPokemon);
+}
+
+
+async function useArrayPokemon(i, arrayPokemon) {
     await renderPokeMinis1stLevel(i, arrayPokemon);
     await stylePokeBgn(i, arrayPokemon);
     await getAbiltiesData(i, arrayPokemon);
+    await changeMiniToBlack(i, arrayPokemon);
+    await noticeDisplayedPokeId(i, arrayPokemon);
+}
+
+
+async function noticeDisplayedPokeId(i, arrayPokemon) {
+    let pokeId = arrayPokemon['id'];
+    loadedPokeIds.push(pokeId);
 }
 
 
@@ -106,16 +128,16 @@ async function renderPokeMinis1stLevel(i, arrayPokemon) {
 
 function generateHTMLPokeMini1st(i, imgSrc, pokeId) {
     return `
-    <button id="pokeButton" class="pokeButton" onclick="switchContent()">
-    <div id="pokeMini${i}" class="pokeMini">
-    <div id="pokeMini1stLine${i}" class="pokeMini1stLine">
-    <div id="pokeMiniId${i}" class="pokeMiniId">${pokeId}</div>
-    <div id="pokeMiniGermanName${i}" class="pokeMiniName"></div>
-    </div>
-    <div id="pokeMiniImgDiv${i}" class="pokeMiniImgDiv">
-    <img src=${imgSrc}>
-    </div>
-    </div>
+    <button id="pokeButton${i}" class="pokeButton" onclick="switchContent()">
+        <div id="pokeMini${i}" class="pokeMini">
+            <div id="pokeMini1stLine${i}" class="pokeMini1stLine">
+                <div id="pokeMiniId${i}" class="pokeMiniId">${pokeId}</div>
+                <div id="pokeMiniGermanName${i}" class="pokeMiniName"></div>
+            </div>
+        <div id="pokeMiniImgDiv${i}" class="pokeMiniImgDiv">
+            <img src=${imgSrc}>
+        </div>
+        </div>
     </button>
     `
 }
@@ -125,7 +147,23 @@ async function renderPokeMinis2ndLevel(i, arrayPokemonSpecies) {
     searchIndexOfGermanData(arrayPokemonSpecies, 'names');
     let germanName = arrayPokemonSpecies['names'][indexOfGermanData]['name'];
     console.log(germanName);
+    await noticeDisplayedPokeName(germanName);
     document.getElementById('pokeMiniGermanName' + i).innerHTML = germanName;
+}
+
+
+async function noticeDisplayedPokeName(germanName) {
+    let pokeName = germanName;
+    loadedPokeNames.push(pokeName);
+}
+
+
+async function changeMiniToBlack(i, arrayPokemon) {
+    let slot1 = arrayPokemon['types'][0]['type']['name'];
+    if (slot1 == 'electric' || slot1 == 'ice') {
+        document.getElementById('pokeMiniGermanName' + i).classList.add(`color-black`);
+        document.getElementById('pokeMiniId' + i).classList.add(`color-black`);
+    }
 }
 
 
@@ -216,5 +254,33 @@ function searchIndexOfGermanData(arrayAsJSON, index) {
             indexOfGermanData = j;
             return indexOfGermanData;
         }
+    }
+}
+
+
+// searchByName
+
+// function searchIndexOfName(searchName) {
+//     for (let i = 0; i < pokes.length; i++) {
+//         let nameGerman = pokes[i]['pokeNameGerman'][0];
+//         if (nameGerman) {
+//             nameGerman = nameGerman.toLowerCase();
+//         }
+//         if (searchName == nameGerman) {
+//             let index = i;
+//             return index;
+//         }
+//     }
+// }
+
+function searchByName() {
+    for (let i = 0; i < loadedPokeNames.length; i++) {
+        let searchName = document.getElementById('searchName').value;
+        searchName = searchName.toLowerCase();
+        let loadedPokeName = loadedPokeNames[i].toLowerCase();
+        let result = loadedPokeName.startsWith(searchName);
+        if (result == true) {
+            console.log(result + i);
+        } else {console.log('sch')}
     }
 }
