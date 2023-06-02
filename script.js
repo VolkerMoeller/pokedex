7ed73d8fb7e12bc36f2502a0ec8d0ae3636b2277
VolkerMoeller <v.moeller@t-online.de>
2023-06-02 09:21:47 +0200

    V 3.0 clean code

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ let currentPokeNr = 0
 let currentGermanName = '';
 let indexOfGermanData;
 let functionRunning = false;
+// for search
 let loadedPokeNames = [0];
 let loadedPokeIds = [0];
+
+// dont't want that?:
 let loadedPokeSlots1 = [0];
 let loadedPokeSlots2 = [0];
 let pokesFavorites = [0];
 let pokesFlavorText = [0];
-let currentArrayPokemon = [];
-let currentArrayPokemonSpecies = [];
 
 
 function init() {
@@ -49,18 +50,6 @@ function updateCountNrs(end) {
 }
 
 
-async function fetchDataFromServer(url) {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
-        throw error;
-    }
-}
-
-
 async function performServerRequests(i) {
     try {
         let url1 = `https://pokeapi.co/api/v2/pokemon/${i}/`;
@@ -73,18 +62,57 @@ async function performServerRequests(i) {
 }
 
 
+function takeDynamikUrl(arrayPokemon) {
+    let dynamicUrlIndex = arrayPokemon['abilities'].length - 1;
+    let dynamicUrl = arrayPokemon['abilities'][dynamicUrlIndex]['ability']['url'];
+    return dynamicUrl;
+}
+
+
+async function fetchAbilitiesDataFromServer(url) {
+    let response = await fetch(url);
+    let arrayPokemonAbilities = await response.json();
+    return arrayPokemonAbilities;
+}
+
+
+async function fetchDataFromServer(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Fehler beim Abrufen der Daten:', error);
+        throw error;
+    }
+}
+
+// useArray
 async function fetchArrayPokemon(i, url1) {
     let arrayPokemon = await fetchDataFromServer(url1);
-    console.log(i + ' pokemonData:', arrayPokemon);
-    currentArrayPokemon = arrayPokemon;
+    await getAbiltiesData(i, arrayPokemon);
     useArrayPokemon(i, arrayPokemon);
 }
 
 
+async function getAbiltiesData(i, arrayPokemon) {
+    let dynamikUrl = await takeDynamikUrl(arrayPokemon);
+    let arrayPokemonAbilities = await fetchAbilitiesDataFromServer(dynamikUrl);
+    useArrayPokemonAbilities(i, arrayPokemonAbilities);
+}
+
+
+
+async function fetchArrayPokemonSpecies(i, url2) {
+    let arrayPokemonSpecies = await fetchDataFromServer(url2);
+    useArrayPokemonSpecies(i, arrayPokemonSpecies);
+}
+
+
 async function useArrayPokemon(i, arrayPokemon) {
+    console.log(i + ' pokemonData:', arrayPokemon);
     await renderPokeMinis1stLevel(i, arrayPokemon);
     await stylePokeBgn(i, arrayPokemon, 'pokeMini');
-    await getAbiltiesData(i, arrayPokemon);
     await changeMiniToBlack(i, arrayPokemon);
     await noticeDisplayedPokeId(arrayPokemon);
     await noticeDisplayedPokeSlots(arrayPokemon);
@@ -94,6 +122,20 @@ async function useArrayPokemon(i, arrayPokemon) {
 }
 
 
+async function useArrayPokemonSpecies(i, arrayPokemonSpecies) {
+    console.log(i + ' pokemon-speciesData:', arrayPokemonSpecies);
+    await getPokeGermanName(arrayPokemonSpecies);
+    await fillPokeWithName(currentGermanName, i)
+    await noticeDisplayedPokeName(currentGermanName);
+}
+
+
+async function useArrayPokemonAbilities(i, arrayPokemonAbilities) {
+    console.log(i + ' pokemon-abitiesData:', arrayPokemonAbilities);
+}
+
+
+// toDo
 async function noticeDisplayedPokeId(arrayPokemon) {
     let pokeId = arrayPokemon['id'];
     pokeId = pokeId.toString();
@@ -127,44 +169,11 @@ async function noticeDisplayedPokeSlot2(i, arrayPokemon) {
 }
 
 
-async function fetchArrayPokemonSpecies(i, url2) {
-    let arrayPokemonSpecies = await fetchDataFromServer(url2);
-    console.log(i + ' pokemon-speciesData:', arrayPokemonSpecies);
-    currentArrayPokemonSpecies = arrayPokemonSpecies;
-    await useArrayPokemonSpecies(i, arrayPokemonSpecies);
-}
-
-
-async function useArrayPokemonSpecies(i, arrayPokemonSpecies) {
-    await getPokeGermanName(arrayPokemonSpecies);
-    await fillPokeWithName(currentGermanName, i)
-    await noticeDisplayedPokeName(currentGermanName);
-}
-
-
 async function fillPokeWithName(currentGermanName, i) {
     document.getElementById('pokeMiniGermanName' + i).innerHTML = currentGermanName;
 }
 
-async function getAbiltiesData(i, arrayPokemon) {
-    let dynamikUrl = await takeDynamikUrl(arrayPokemon);
-    let arrayPokemonAbilities = await fetchAbilitiesDataFromServer(dynamikUrl);
-    console.log(i + ' pokemon-abitiesData:', arrayPokemonAbilities);
-}
 
-
-function takeDynamikUrl(arrayPokemon) {
-    let dynamicUrlIndex = arrayPokemon['abilities'].length - 1;
-    let dynamicUrl = arrayPokemon['abilities'][dynamicUrlIndex]['ability']['url'];
-    return dynamicUrl;
-}
-
-
-async function fetchAbilitiesDataFromServer(url) {
-    let response = await fetch(url);
-    let arrayPokemonAbilities = await response.json();
-    return arrayPokemonAbilities;
-}
 
 
 // render PokeMinis
@@ -175,21 +184,7 @@ async function renderPokeMinis1stLevel(i, arrayPokemon) {
 }
 
 
-function generateHTMLPokeMini1st(i, imgSrc, pokeId) {
-    return /*html*/`
-    <button id="pokeButton${i}" class="pokeButton" onclick="showPokeCard(${i})">
-        <div id="pokeMini${i}" class="pokeMini">
-            <div id="pokeMini1stLine${i}" class="pokeMini1stLine">
-                <div id="pokeMiniId${i}" class="pokeMiniId">${pokeId}</div>
-                <div id="pokeMiniGermanName${i}" class="pokeMiniName"></div>
-            </div>
-            <div id="pokeMiniImgDiv${i}" class="pokeMiniImgDiv">
-                <img src=${imgSrc}>
-            </div>
-        </div>
-    </button>
-    `
-}
+
 
 function showPokeCard(i) {
     switchContent();
@@ -351,55 +346,7 @@ async function renderPokeCards1stLevel(i) {
     document.getElementById('pokeCardPlace').innerHTML += generateHTMLPokeCard(i);
 }
 
-function generateHTMLPokeCard(i) {
-    return /*html*/`
-    <div id="pokedex${i}" class="pokedex display-none">
-        <div id="pokedex-top${i}" class="pokedex-top">
-            <div>
-                <button onclick="switchContent()" class="btn-back">
-                    <img src="./img/backspace.png">
-                </button>
-            </div>
-            <div class="pokedex-above">
-                <div id="pokedex-name${i}" class="pokedex-name">
-                </div>
-                <div id="pokedex-id${i}" class="pokedex-id">
-                </div>
-            </div>
-            <div class="slot-line">
-                <div id="pokedex-slots${i}" class="pokedex-slots">
-                </div>
-                <div class="favorite">
-                    <div id="fill0${i}"><button id="btn-fill0${i}" class="btn-fav" onclick="setFavorite(${i})"><img id="pokeImgFavFill0${i}" src="./img/favorite_FILL0.png"></button></div>
-                    <div id="fill1${i}" class="display-none"><button id="btn-fill1${i}"class="btn-fav" onclick="setFavorite(${i})"><img id="pokeImgFavFill1${i}" src="./img/favorite_FILL1.png"></button></div>
-                </div>
-            </div>
-            <div id="pokedex-image-place${i}" class="pokedex-image-place">
-                <div id="pokedex-image${i}" class="pokedex-image"></div>
-            </div>
-        </div>
-        <div id="pokedex-bottom${i}" class="pokedex-bottom">
-        <div class="navigationPoke">
-            <div onmouseover="hoverNavigationOver(1, ${i})" onmouseout="hoverNavigationOut(1, ${i})">
-                <button onclick="showCurrentCardById('card1${i}', ${i}, cardIdsPokeAll)" id="btn-card1${i}">About</button>
-            </div>
-            <div onmouseover="hoverNavigationOver(2, ${i})" onmouseout="hoverNavigationOut(2, ${i})">
-                <button onclick="showCurrentCardById('card2${i}', ${i}, cardIdsPokeAll)" id="btn-card2${i}">Base Stats</button>
-            </div>
-            <div onmouseover="hoverNavigationOver(3, ${i})" onmouseout="hoverNavigationOut(3, ${i})">
-                <button onclick="showCurrentCardById('card3${i}', ${i}, cardIdsPokeAll)" id="btn-card3${i}">Evolution</button>
-            </div>
-            <div onmouseover="hoverNavigationOver(4, ${i})" onmouseout="hoverNavigationOut(4, ${i})">
-                <button onclick="showCurrentCardById('card4${i}', ${i}, cardIdsPokeAll)" id="btn-card4${i}">5 Moves</button>
-            </div>
-        </div>
-        <div id="card1${i}" class="cards"></div>
-        <div id="card2${i}" class="cards display-none"></div>
-        <div id="card3${i}" class="cards display-none"></div>
-        <div id="card4${i}" class="cards display-none"></div>
-    </div> 
-    `
-}
+
 
 // favorites
 function setFavorite(i) {
